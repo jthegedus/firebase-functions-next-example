3856d652229e82fad9bfd871deae6451ce035a6b
James Hegedus <jthegedus@jttech.io>
2017-07-05 01:02:09 +1000

    add express custom Next.js server

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,7 +1,8 @@
 const functions = require("firebase-functions")
 const next = require("next")
+const express = require("express")
 
-var app, handle
+var app, handle, server
 
 // FIXME: Firebase deploy command tries to load the code, causing an error,
 // so, we delay app initialization
@@ -10,8 +11,27 @@ if (!/\/tmp/.test(__dirname)) {
   // (Same problem reported at https://github.com/zeit/next.js/issues/2123)
   app = next({ dev: false, conf: { assetPrefix: "/next", distDir: "next" } })
   handle = app.getRequestHandler()
+
+  app.prepare().then(() => {
+    server = express()
+
+    server.get("_next/*", (req, res) => {
+      console.log("next")
+      return handle(req, res)
+    })
+
+    server.get("_webpack/*", (req, res) => {
+      console.log("webpack")
+      return handle(req, res)
+    })
+
+    server.get("*", (req, res) => {
+      return handle(req, res)
+    })
+  })
 }
 
-exports.next = functions.https.onRequest((req, res) => {
-  return app.prepare().then(() => handle(req, res))
-})
+exports.next = functions.https.onRequest(server)
+// exports.next = functions.https.onRequest((req, res) => {
+//   return app.prepare().then(() => handle(req, res))
+// })
