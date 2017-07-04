bfdd67b2a568753850e2980266840dabf27edc05
James Hegedus <jthegedus@jttech.io>
2017-07-05 02:42:52 +1000

    Serve all content successfully from Cloud Functions

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,37 +1,18 @@
 const functions = require("firebase-functions")
 const next = require("next")
-const express = require("express")
 
-var app, handle, server
+var app, handle
 
 // FIXME: Firebase deploy command tries to load the code, causing an error,
 // so, we delay app initialization
 if (!/\/tmp/.test(__dirname)) {
   // NOTE: Dev mode doesn't work like a charm :/
   // (Same problem reported at https://github.com/zeit/next.js/issues/2123)
-  app = next({ dev: false, conf: { assetPrefix: "/next", distDir: "next" } })
+  app = next({ dev: false, conf: { distDir: "next" } })
   handle = app.getRequestHandler()
-
-  app.prepare().then(() => {
-    server = express()
-
-    server.get("_next/*", (req, res) => {
-      console.log("next")
-      return handle(req, res)
-    })
-
-    server.get("_webpack/*", (req, res) => {
-      console.log("webpack")
-      return handle(req, res)
-    })
-
-    server.get("*", (req, res) => {
-      return handle(req, res)
-    })
-  })
 }
 
-exports.next = functions.https.onRequest(server)
-// exports.next = functions.https.onRequest((req, res) => {
-//   return app.prepare().then(() => handle(req, res))
-// })
+exports.next = functions.https.onRequest((req, res) => {
+  console.log("File: " + req.originalUrl)
+  return app.prepare().then(() => handle(req, res))
+})
