cc5341d9b82db1b14ff866aecef5528c4904164c
jthegedus <jthegedus@jttech.io>
2018-01-04 00:26:35 +1100

    Fix Header component className

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,10 +3,10 @@ import Link from "next/link"
 export default ({ pathname }) => (
   <header>
     <Link href="/">
-      <a className={pathname === "/" && "is-active"}>Home</a>
+      <a className={pathname === "/" ? "is-active" : ""}>Home</a>
     </Link>{" "}
     <Link href="/about">
-      <a className={pathname === "/about" && "is-active"}>About</a>
+      <a className={pathname === "/about" ? "is-active" : ""}>About</a>
     </Link>
   </header>
 )
